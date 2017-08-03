3f2a644e670d20ee348d6e8f7dbe7562d1b80c01
Tiyofirdhani <tiyofirdhani@gmail.com>
2017-08-03 08:59:23 +0700

    new

--- application/models/berkasmodel.php
+++ application/models/berkasmodel.php
@@ -1,96 +1,91 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Berkasmodel extends CI_Model {
+class BerkasModel extends CI_Model {
 
 	public function getJson($jenis)
 	{
 		$page = isset($_POST['page']) ? intval($_POST['page']) : 1;
         $rows = isset($_POST['rows']) ? intval($_POST['rows']) : 10;
-        $sort = isset($_POST['sort']) ? strval($_POST['sort']) : 'id_berkas';
+        $sort = isset($_POST['sort']) ? strval($_POST['sort']) : 'ID_BERKAS';
         $order = isset($_POST['order']) ? strval($_POST['order']) : 'asc';
         $offset = ($page-1) * $rows;
         $this->limit = $rows;
         $this->offset = $offset;
-        //searching
+
         $searchKey=isset($_POST['searchKey']) ? strval($_POST['searchKey']) : '';
 		$searchValue=isset($_POST['searchValue']) ? strval($_POST['searchValue']) : '';
 
-        if ($jenis=='total') {
-        	$result = $this->db->query("select * from berkas")->num_rows();
+		if ($jenis=='total') {
+
+        	$result = $this->db->query("select * from TBL_BERKAS")->num_rows();
         	return $result;
+
         } elseif ($jenis=='rows') {
+
         	$this->db->limit($rows,$offset);
         	$this->db->order_by($sort,$order);
-			$this->db->select("a.id_berkas, convert(varchar(20),a.tgl_terima,120) as tgl_terima_desc, a.penerima_berkas, a.pemilik_berkas, a.kode_bagian, a.isi_berkas, b.nama_bagian bagian_desc, c.nama_lengkap penerima_berkas_desc, d.nama_lengkap pemilik_berkas_desc");
-			$this->db->from("berkas a");
-			$this->db->join("(SELECT left(kode_jabatan,4) as kode, nama_bagian FROM bagian group by left(kode_jabatan,4), nama_bagian) b", "a.kode_bagian = b.kode");
-			$this->db->join("karyawan c", "a.penerima_berkas=c.nip");
-			$this->db->join("karyawan d", "a.pemilik_berkas=d.nip");
-        	if($searchKey<>''){
-				$this->db->where($searchKey." like '%".$searchValue."%'");	
-			}
-        	$hasil=$this->db->get ('',$this->limit, $this->offset)->result_array();
-        	return $hasil;
+			$this->db->select("a.*");
+			$this->db->from("TBL_BERKAS a");
+
+        if($searchKey<>''){
+			$this->db->where($searchKey." like '%".$searchValue."%'");
+		}
+
+        $hasil=$this->db->get ('',$this->limit, $this->offset)->result_array();
+        return $hasil;
     	}
 	}
 
-	public function tambah_berkas(){
+	public function newData()
+	{
+		$JENIS_EKSPEDISI = htmlspecialchars($_REQUEST['JENIS_EKSPEDISI']);
 
-		$penerima_berkas = htmlspecialchars($_REQUEST['penerima_berkas']);
-		$pemilik_berkas = htmlspecialchars($_REQUEST['pemilik_berkas']);
-		$kode_bagian = htmlspecialchars($_REQUEST['kode_bagian']);
-		$isi_berkas = htmlspecialchars($_REQUEST['isi_berkas']);
-
-		$id_berkas = $this->db->query("select dbo.getNomorDokumen() as baru")->row_array();
-		$tgl_terima = $this->db->query("select getDate() as baru")->row_array();
+		//$ID_BERKAS = $this->db->query("select dbo.getNomorJenisEkspedisi() as baru")->row_array();
 
 		$data = array(
-		        'id_berkas' => $id_berkas['baru'],
-		        'tgl_terima' => $tgl_terima['baru'],
-		        'penerima_berkas' => $penerima_berkas,
-		        'pemilik_berkas' => $pemilik_berkas,
-		        'kode_bagian' => $kode_bagian,
-		        'isi_berkas' => $isi_berkas,
+		       // 'ID_BERKAS' => $ID_BERKAS['baru'], -- identity
+		        'JENIS_EKSPEDISI' => $JENIS_EKSPEDISI
 		);
 
-		if ($this->db->insert('berkas', $data)) {
-			return "success";
+		if ($this->db->insert('TBL_BERKAS', $data)) {
+			return "SUCCESS";
 		} else {
-			return "insert failed";
+			return "FAILED";
 		}
 	}
 
-	public function hapus_berkas($id_berkas)
+	public function updateData($ID_BERKAS)
 	{
-		$this->db->where('id_berkas', $id_berkas);
-		if ($this->db->delete('berkas')) {
-			return "success";
-		} else {
-			return "delete failed";
-		}
-	}
-
-	public function update_berkas($id_berkas)
-	{
-		$penerima_berkas = htmlspecialchars($_REQUEST['penerima_berkas']);
-		$pemilik_berkas = htmlspecialchars($_REQUEST['pemilik_berkas']);
-		$kode_bagian = htmlspecialchars($_REQUEST['kode_bagian']);
-		$isi_berkas = htmlspecialchars($_REQUEST['isi_berkas']);
+		$JENIS_EKSPEDISI = htmlspecialchars($_REQUEST['JENIS_EKSPEDISI']);
 
 		$data = array(
-		        'penerima_berkas' => $penerima_berkas,
-		        'pemilik_berkas' => $pemilik_berkas,
-		        'kode_bagian' => $kode_bagian,
-		        'isi_berkas' => $isi_berkas,
+		        'JENIS_EKSPEDISI' => $JENIS_EKSPEDISI
 		);
 
-		$this->db->where('id_berkas', $id_berkas);
+		$this->db->where('ID_BERKAS', $ID_BERKAS);
 
-		if ($this->db->update('berkas', $data)) {
-			return "success";
+		if ($this->db->update('TBL_BERKAS', $data)) {
+			return "SUCCESS";
 		} else {
-			return "update failed";
+			return "FAILED";
 		}
 	}
+
+	public function deleteData($ID_BERKAS)
+	{
+		$this->db->where('ID_BERKAS', $ID_BERKAS);
+
+		if ($this->db->delete('TBL_BERKAS')) {
+			$result['error']=false;
+		} else {
+			$result['error']=true;
+		}
+		
+		return $result;
+	}
+
 }
+
+/* End of file berkasModel.php */
+/* Location: ./application/models/berkasModel.php */
