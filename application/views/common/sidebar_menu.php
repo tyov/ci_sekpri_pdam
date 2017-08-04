9b3194973a50b62c1b7281161d94bc67159ae7d1
Tiyofirdhani <tiyofirdhani@gmail.com>
2017-08-04 13:24:00 +0700

    delete

--- application/views/common/sidebar_menu.php
+++ application/views/common/sidebar_menu.php
@@ -1,28 +1,4 @@
 <ul id="tt" class="easyui-tree">
-  <!--  <li>
-        <span>Berkas</span>
-        <ul>
-            <li><span>Berkas</span></li>
-            <li><span>Ekspedisi</span></li>
-            <li><span>Master Ekspedisi</span></li>
-        </ul>
-    </li>
-    <li>
-        <span>Agenda</span>
-        <ul>
-            <li><span>Agenda</span></li>
-            <li><span>Ekspedisi</span></li>
-            <li><span>Master Agenda</span></li>
-        </ul>
-    </li>
-    <li>
-        <span>Laporan</span>
-        <ul>
-            <li><span>Laporan</span></li>
-            <li><span>Ekspedisi</span></li>
-            <li><span>Master Laporan</span></li>
-        </ul>
-    </li>-->
 </ul>
 
 <script type="text/javascript">
