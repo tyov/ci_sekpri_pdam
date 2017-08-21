4088542b0e525f15ad674ac1824f49efa1e6b50f
dwilidiana <dwi.lidiana18@gmail.com>
2017-08-21 15:31:39 +0700

    CIMOL
    ENAKS

--- application/controllers/laporanAgenda.php
+++ application/controllers/laporanAgenda.php
@@ -22,40 +22,42 @@ class LaporanAgenda extends CI_Controller {
 		<htmlpagefooter name="MyFooter1">
 			<table width="100%" style="vertical-align: bottom; font-family: serif; font-size: 8pt; color: #000000; font-weight: bold; font-style: italic;">
 				<tr>
-					<td width="33%" align="center" style="font-weight: bold; font-style: italic;">eOffice PDAM Malang - Laporan Persuratan, Halaman {PAGENO} dari {nbpg}</td>
+					<td width="33%" align="center" style="font-weight: bold; font-style: italic;">eOffice PDAM Malang - Laporan Semua Kegiatan SEKPRI, BULAN {PAGENO} dari {nbpg}</td>
 				</tr>
 			</table>
 		</htmlpagefooter>
 		<sethtmlpagefooter name="MyFooter1" value="on" />
 		<div style="font-size:20px; font-weight:bold">PDAM KOTA MALANG</div>
 		<div style="font-weight:bold;">Jl. Terusan Danau Sentani No.100 - Malang</div>
-		<div style="font-size:20px; font-weight:bold; text-align:center">LAPORAN PERSURATAN</div>';
+		<div style="font-size:20px; font-weight:bold; text-align:center">Laporan Semua Kegiatan SEKPRI</div>';
 		$html .='
 		<table width="100%" border="1" cellspacing="0" cellpadding="2">
 		  <tr>
-			<td width="5%" align="center"><strong>No</strong></td>
-			<td width="16%" align="center"><strong>No Surat</strong></td>
-			<td width="8%" align="center"><strong>Tanggal</strong></td>
-			<td width="8%" align="center"><strong>Tipe Surat</strong></td>
-			<td width="9%" align="center"><strong>Jenis Surat</strong></td>
-			<td width="10%" align="center"><strong>Bagian</strong></td>
-			<td width="13%" align="center"><strong>Tujuan</strong></td>
-			<td width="31%" align="center"><strong>Perihal</strong></td>
+			<td width="10%" align="center"><strong>Tgl Pesan</strong></td>
+			<td width="16%" align="center"><strong>Jenis Kegiatan</strong></td>
+			<td width="10%" align="center"><strong>Asal Kegiatan</strong></td>
+			<td width="10%" align="center"><strong>Pemesan</strong></td>
+			<td width="15%" align="center"><strong>Keterangan</strong></td>
+			<td width="8%" align="center"><strong>Jumlah</strong></td>
+			<td width="10%" align="center"><strong>Ruangan</strong></td>
+			<td width="10%" align="center"><strong>Tgl Mulai</strong></td>
+			<td width="10%" align="center"><strong>Tgl Selesai</strong></td>
 		  </tr>';
-		$no= 1;
+
 		$data = $this->laporanAgendaModel->getJson();
 		foreach($data as $row){
 		//for($x=1; $x<=10; $x++){
 		$html .='  
 		  <tr>
-			<td align="center">'.$no++.'</td>
 			<td>'.$row->TGL_PEMESANAN.'</td>
 			<td>'.$row->JENIS_KEGIATAN.'</td>
 			<td>'.$row->ASAL_KEGIATAN.'</td>
-			<td>'.$row->PEMESANAN.'</td>
+			<td>'.$row->PEMESAN.'</td>
 			<td>'.$row->KETERANGAN.'</td>
-			<td>Tes 1</td>
-			<td>Tes 1</td>
+			<td>'.$row->JUMLAH.'</td>
+			<td>'.$row->RUANG_RAPAT.'</td>
+			<td>'.$row->TGL_MULAI.'</td>
+			<td>'.$row->TGL_SELESAI.'</td>
 		</tr>';
 		}
 		$html .= '</table>';
