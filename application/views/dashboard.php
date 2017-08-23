2ce9b9242823de231396696443b6b83a720c320f
Tiyofirdhani <tiyofirdhani@gmail.com>
2017-08-23 15:02:51 +0700

    lul

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -1,13 +1,13 @@
     <div id="aa" class="easyui-accordion" style="width:100%;height:100%;">
-        <div title="Title1" data-options="iconCls:'icon-save'" style="overflow:auto;padding:10px;">
+        <div title="Agenda" data-options="iconCls:'icon-date',selected:true" style="overflow:auto;padding:10px;">
             <h3 style="color:#0099FF;">Accordion for jQuery</h3>
             <p>Accordion is a part of easyui framework for jQuery. 
             It lets you define your accordion component on web page more easily.</p>
         </div>
-        <div title="Title2" data-options="iconCls:'icon-reload',selected:true" style="padding:10px;">
+        <div title="Rapat" data-options="iconCls:'icon-man'" style="padding:10px;">
             content2
         </div>
-        <div title="Title3">
+        <div title="Berkas" data-options="iconCls:'icon-file'">
             content3
         </div>
     </div>
